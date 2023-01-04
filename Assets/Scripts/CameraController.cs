73ff3fec65ab3acf29e4831130547bdfc4853e8d
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2023-01-04 19:18:08 +0000

    CinemachineUpdate

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+
+    private CinemachineVirtualCamera[] _vCams;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
