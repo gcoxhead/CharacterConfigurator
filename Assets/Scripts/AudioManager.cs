01dc46fcd2ec97cab1a7aaac50a7f2fef0461222
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2023-01-05 21:31:42 +0000

    Audio Manager Update
    Added an Audio Manager Script and called play methods from Gui buttons on the customiser.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource SceneMusic;
+    public AudioSource Sword;
+    public AudioSource Shield;
+    public AudioSource Wind;
+
+    public void playSword()
+    {
+        Sword.Play();
+    }
+
+    public void playShield()
+    {
+        Shield.Play();
+    }
+
+    public void playWind()
+    {
+        Wind.Play();
+    }
+
+    public void playMusic()
+    {
+        SceneMusic.Play();
+    }
+
+}
+
