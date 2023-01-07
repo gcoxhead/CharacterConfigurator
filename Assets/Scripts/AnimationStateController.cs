0e40c157bec5b07f1fba18300d45cc1e8a624ba6
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2023-01-07 16:51:31 +0000

    Animation Updates
    Added animation controller script, as well as animator.

---
+++ Assets/Scripts/AnimationStateController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            animator.SetBool("isRunning", true);
+            Debug.Log("Run forest, run!");
+        }
+        if (!Input.GetKey("up"))
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            animator.SetBool("isBackwards", true);
+        }
+        if (!Input.GetKey("down"))
+        {
+            animator.SetBool("isBackwards", false);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            animator.SetBool("isJumping", true);
+        }
+
+        if (!Input.GetKey("space"))
+        {
+            animator.SetBool("isJumping", true);
+        }
+    }
+}
+
+
